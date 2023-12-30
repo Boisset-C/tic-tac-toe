b74bd5ad35aab04c79310cc82ef7fb901ca25434
cosme boisset <cosmeboisset@cosmes-MacBook-Pro.local>
2023-12-30 17:22:54 -0600

    Created game logic.

--- script.js
+++ script.js
@@ -14,17 +14,45 @@ function createGameBoard() {
 	for (let i = 0; i < rows; i++) {
 		board[i] = [];
 		for (let j = 0; j < columns; j++) {
-			board[i].push([]);
+			board[i][j] = "";
 		}
 	}
 
 	return board;
 }
 
-console.log(createGameBoard());
+let gameBoard = createGameBoard();
+console.log(gameBoard);
 
 // player one { name: john smith, score: score, move: X }
 // player two { name: anne smith, score: score, move: O }
+function createPlayer(name, symbol) {
+	return { name, symbol };
+}
+
+let john = createPlayer("John", "X");
+let anne = createPlayer("Anne", "0");
+
+function makeMove(board, row, column, symbol) {
+	if (board[row][column] === "") {
+		board[row][column] = symbol;
+	} else {
+		return "Please pick another square";
+	}
+}
+
+function game(board, playerOne, playerTwo) {
+	const newBoard = board;
+	const firstPlayer = playerOne;
+	const secondPlayer = playerTwo;
+
+	makeMove(newBoard, 1, 2, firstPlayer.symbol);
+	makeMove(newBoard, 1, 1, secondPlayer.symbol);
+
+	return newBoard;
+}
+
+console.log("This is game board after one move", game(gameBoard, john, anne));
 
 //logic to keep track of turn and player move
 // moves will involve a 0 and X
