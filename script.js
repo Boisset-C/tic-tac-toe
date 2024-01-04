2216474191860bb52d19234c0ddfbf1a921c5c51
cosme boisset <cosmeboisset@cosmes-MacBook-Pro.local>
2024-01-04 14:31:22 -0600

    Added base game logic and UI.

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+// Keeps track of signed in players
+function PlayerManager() {
+	let players = { playerOne: null, playerTwo: null };
+
+	return {
+		storePlayer: function (player, formId) {
+			if (formId === "player1") {
+				players.playerOne = player;
+			} else if (formId === "player2") {
+				players.playerTwo = player;
+			}
+
+			console.log(players);
+
+			if (players.playerOne && players.playerTwo) {
+				console.log("Both players are set:", players);
+				game(players);
+			}
+		},
+		getPlayers: function () {
+			return players;
+		},
+	};
+}
+
+const playerManager = PlayerManager();
+
+// Creates player
+function createPlayer(name, symbol) {
+	return { name, symbol, score: 0, isTurn: false };
+}
+
+//Keeps track of current player move
+function createTurnManager() {
+	let currentPlayer = null;
+
+	return {
+		setPlayer(player) {
+			currentPlayer = player;
+			player.isTurn = true;
+		},
+		switchPlayer(playerOne, playerTwo) {
+			currentPlayer.isTurn = false;
+			currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+			currentPlayer.isTurn = true;
+		},
+		getCurrentPlayer() {
+			return currentPlayer;
+		},
+	};
+}
+
+// player creation
+function handleCreatePlayer(e) {
+	e.preventDefault();
+
+	//determine which player is being created
+	const playerBeingCreated = e.target.id;
+	console.log("This is form ID", playerBeingCreated);
+	//retrieve form values
+	const username = document.getElementById(
+		`username${playerBeingCreated.charAt(playerBeingCreated.length - 1)}`
+	).value;
+
+	const symbol = document.querySelector(
+		`input[name="symbol${playerBeingCreated.charAt(
+			playerBeingCreated.length - 1
+		)}"]:checked`
+	).value;
+
+	let player = createPlayer(username, symbol);
+	playerManager.storePlayer(player, playerBeingCreated);
+
+	console.log(player);
+
+	//hide form
+	document.getElementById(playerBeingCreated).classList.add("d-none");
+
+	//show player info
+	const currentPlayerInfo =
+		document.getElementById(playerBeingCreated).nextElementSibling;
+	currentPlayerInfo.classList.remove("d-none");
+
+	//add player name
+	const currentUsername = document.createElement("p");
+	currentUsername.className = "name";
+	currentUsername.textContent = `name: ${player.name}`;
+	currentPlayerInfo.appendChild(currentUsername);
+
+	//add player symbol
+	const currentPlayerSymbol = document.createElement("p");
+	currentPlayerSymbol.className = "symbol";
+	currentPlayerSymbol.textContent = `symbol: ${player.symbol}`;
+	currentPlayerInfo.appendChild(currentPlayerSymbol);
+
+	//add player score
+	const currentPlayerScore = document.createElement("p");
+	currentPlayerScore.className = "score";
+	currentPlayerScore.textContent = `score: ${player.score}`;
+	currentPlayerInfo.appendChild(currentPlayerScore);
+}
+
+document
+	.getElementById("player1")
+	.addEventListener("submit", handleCreatePlayer);
+document
+	.getElementById("player2")
+	.addEventListener("submit", handleCreatePlayer);
+
 //Tic Tac Toe
 /*
 [[],[],[]]
@@ -5,58 +114,48 @@
 [[],[],[]]
 */
 // 9 X 9 grid to create board
-function createGameBoard() {
+function createGameBoard(turnManager, playerOne, playerTwo) {
 	const gameContainer = document.getElementById("game-container");
-	const rows = 3;
-	const columns = 3;
+	const rows = 4;
+	const columns = 4;
 
 	//creates board should be a 3 * 3 square
-	for (let i = 0; i < rows; i++) {
+	for (let i = 1; i < rows; i++) {
 		const row = document.createElement("div");
 		row.classList.add("row");
 		gameContainer.appendChild(row);
-		for (let j = 0; j < columns; j++) {
-			const cell = document.createElement("div");
+		for (let j = 1; j < columns; j++) {
+			const cell = document.createElement("p");
 			cell.classList.add("column");
 			cell.textContent = "";
+			cell.id = `row${i}col${j}`;
+			//add event listener to each cell
+			cell.addEventListener("click", function () {
+				handlePlayerMove(cell, turnManager, playerOne, playerTwo);
+			});
+
 			row.appendChild(cell);
 		}
 	}
 }
 
-createGameBoard();
+//Allow player to move
+function handlePlayerMove(cell, turnManager, playerOne, playerTwo) {
+	let currentPlayer = turnManager.getCurrentPlayer();
 
-// player one { name: john smith, score: score, move: X }
-// player two { name: anne smith, score: score, move: O }
-function createPlayer(name, symbol) {
-	return { name, symbol };
-}
-
-let john = createPlayer("John", "X");
-let anne = createPlayer("Anne", "0");
-
-function makeMove(board, row, column, symbol) {
-	if (board[row][column] === "") {
-		board[row][column] = symbol;
+	if (cell.textContent === "") {
+		cell.textContent = currentPlayer.symbol;
+		turnManager.switchPlayer(playerOne, playerTwo);
 	} else {
-		return "Please pick another square";
+		console.log("Cell has already been filled.");
 	}
 }
 
-// function game(board, playerOne, playerTwo) {
-// 	const newBoard = board;
-// 	const firstPlayer = playerOne;
-// 	const secondPlayer = playerTwo;
+function game() {
+	const { playerOne, playerTwo } = playerManager.getPlayers();
 
-// 	makeMove(newBoard, 1, 2, firstPlayer.symbol);
-// 	makeMove(newBoard, 1, 1, secondPlayer.symbol);
+	let turnManager = createTurnManager();
 
-// 	return newBoard;
-// }
-
-// console.log("This is game board after one move", game(gameBoard, john, anne));
-
-//logic to keep track of turn and player move
-// moves will involve a 0 and X
-//turn based keep track of player and turn
-//Winning condition when three X's or rows
+	turnManager.setPlayer(playerOne); // set initial player
+	createGameBoard(turnManager, playerOne, playerTwo);
+}
